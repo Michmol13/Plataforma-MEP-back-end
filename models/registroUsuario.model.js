abf63fa45aeb5ee13561218af6840a5437e0b944
Brandon1606 <bhernandezs@ucenfotec.ac.cr>
2025-03-31 21:40:25 -0600

    model Brandon

--- models/registroUsuario.model.js
+++ models/registroUsuario.model.js
@@ -1,11 +1,49 @@
 const mongoose = require('mongoose');
 
-const usuarioSchema = new mongoose.Schema({
+const UsuariosuarioSchema = new mongoose.Schema({
+    
+    nombre:{
+        type:String,
+        require: true, 
+        unique: false
+
+
+    },
     cedula: {
         type: String,
         unique: true,
         required: true,
         match: [/^\d{1}-\d{4}-\d{4}$/, "Formato de cédula inválido"] // Valida el formato X-XXXX-XXXX
+    },
+    correoElectronico: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/, "Correo electrónico inválido"] // Valida formato de email
+    },
+    contrasena: {
+        type: String,
+        required: true,
+        minlength: 8 // Mínimo 8 caracteres
+    },
+    confirmarContrasena: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value === this.contrasena; // Debe coincidir con la contraseña
+            },
+            message: "Las contraseñas no coinciden"
+        }
+    },
+    rol: {
+        type: String,
+        enum: ["Maestro", "Padre de familia"], // Lista desplegable con opciones fijas
+        required: true
+    },
+    estadoCuenta: {
+        type: Boolean,
+        default: true // Activo/Inactivo (true = Activo, false = Inactivo)
     }
 });
 
