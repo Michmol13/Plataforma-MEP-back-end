6bec5baa2701a5cd3ece1c6eaa3efd9e4ae75002
Scarleth160102 <sgonzalezv@ucenfotec.ac.cr>
2025-03-31 20:55:09 -0600

    Parte1-Scarleth

--- models/categoriaMateriales.js
+++ models/categoriaMateriales.js
@@ -1,6 +1,10 @@
 const mongoose = require ('mongoose');
 const schemaCategoriaMateriales = new mongoose.Schema ({
 
-    
+    nombre:{ 
+    type:String ,
+    required: true },
+    descripcion: { type: String }
 
 })
+module.exports = mongoose.model('Categoria', CategoriaSchema);
